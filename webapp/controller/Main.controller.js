67781898ca4594c6694504d55dfcc88b0424cf2d
FabricioBe <fabricio.beneduzi@gmail.com>
2021-12-15 00:38:05 +0000

    Primeira Alteracao

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
 
         return Controller.extend("AppFioriAlunoXX.appfiorialunoxx.controller.Main", {
             onInit: function () {
-
+                    /* Teste */
             }
         });
     });
