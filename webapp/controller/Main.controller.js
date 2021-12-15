471bf471c43dde98e9fd6c64ec5b065c1f143785
FabricioBe <fabricio.beneduzi@gmail.com>
2021-12-15 15:38:43 +0000

    Primeiro Codigo

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,16 +1,51 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator", 
+    "sap/m/BusyDialog"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel,MessageBox,Filter,FilterOperator,BusyDialog) {
         "use strict";
 
         return Controller.extend("AppFioriAlunoXX.appfiorialunoxx.controller.Main", {
+
             onInit: function () {
-                    /* Teste */
-                /* Teste 2 */
-            }
+                this.oGlobalArray   = []
+                this.oExcludedArray = []
+                this.oFieldScreen   = new JSONModel({ btnsVisible : false, btnEditVisible : true });
+                this.oConsModel     = new JSONModel({ CadastroSet : [], aExcluded : []});
+                this.BusyDialog     = new BusyDialog( )              
+                 
+                this.getView().setModel(this.oCadastroModel,"oCadastrosModel"); 
+                this.getView().setModel(this.oFieldScreen,"oFieldScreen");
+                this.oConsModel.updateBindings(true);
+                this.oFieldScreen.updateBindings(true);    
+            },
+
+            onSearch:function(oEvent){
+			
+			
+                let Cpf       = oEvent.getParameter('selectionSet')[0].getValue().length ? oEvent.getParameter('selectionSet')[0].getValue()     : undefined
+                let Nome      = oEvent.getParameter('selectionSet')[1].getValue().length ? oEvent.getParameter('selectionSet')[1].getValue()     : undefined
+                let Sobrenome = oEvent.getParameter('selectionSet')[2].getValue().length ? oEvent.getParameter('selectionSet')[2].getValue()     : undefined
+                let DataNasc  = oEvent.getParameter('selectionSet')[3].getDateValue()    ? oEvent.getParameter('selectionSet')[3].getDateValue() : undefined
+
+                let aFilter   = []
+                if(Cpf)       aFilter.push( new Filter('Cpf', FilterOperator.Contains, Cpf))
+                if(Nome)      aFilter.push( new Filter('Nome', FilterOperator.Contains, Nome))
+                if(Sobrenome) aFilter.push( new Filter('Sobrenome', FilterOperator.Contains, Sobrenome))
+                if(DataNasc)  aFilter.push( new Filter('DataNascimento', FilterOperator.EQ, DataNasc))
+
+                this.getView().byId('idCadastroTable').getBinding('items').filter(aFilter)
+                this.getView().byId('idCadastroTable').updateBindings(true)
+                //this.getOwnerComponent().getModel('oCadastroModel').getBindings('/CadastroSet')[0].filter(aFilter)
+                //this.getOwnerComponent().getModel('oCadastroModel').updateBindings(true)
+            },
+
         });
     });
