e1dcf332aad060f2f543a01b2c5c8140e7cdcb4e
FabricioBe <fabricio.beneduzi@gmail.com>
2021-12-16 03:10:53 +0000

    Adicao de dados para edicao

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -28,8 +28,7 @@ sap.ui.define([
             },
 
             onSearch:function(oEvent){
-			
-			
+	
                 let Cpf       = oEvent.getParameter('selectionSet')[0].getValue().length ? oEvent.getParameter('selectionSet')[0].getValue()     : undefined
                 let Nome      = oEvent.getParameter('selectionSet')[1].getValue().length ? oEvent.getParameter('selectionSet')[1].getValue()     : undefined
                 let Sobrenome = oEvent.getParameter('selectionSet')[2].getValue().length ? oEvent.getParameter('selectionSet')[2].getValue()     : undefined
@@ -49,28 +48,67 @@ sap.ui.define([
 
             onAdd : function(oEvent){
 
-                this._dialogBaixaEstoque = sap.ui.xmlfragment("view.Cadastro", this)
+                this.oCadastroModelLine = new sap.ui.model.json.JSONModel({ Cpf: '', Nome: '', Sobrenome: '', Email:'', Telefone:'', DataNascimento : new Date()})
+                this.getView().setModel(this.oCadastroModelLine, 'oCadastroModelLine')
+                this.oCadastroModelLine.updateBindings(true)
+
+                this.onOpenDialog()
+            },
+
+            onEdit : function(oEvent){
+                
+                let aSelectedContexts = this.getView().byId('idCadastroTable').getSelectedContextPaths();
+                
+                if(aSelectedContexts.length){
+                    let oModel = this.getView().getModel('oCadastroModel').getObject(aSelectedContexts[0])
+                    this.oCadastroModelLine = new sap.ui.model.json.JSONModel(oModel)
+                    this.getView().setModel(this.oCadastroModelLine, 'oCadastroModelLine')
+                    this.oCadastroModelLine.updateBindings(true)
+                    this.onOpenDialog()
+                } 
+            },
+
+            onOpenDialog : function(){
+                this._dialogCadastro = sap.ui.xmlfragment("AppFioriAlunoXX.appfiorialunoxx.view.Cadastro", this)
     			if (this.getView().$().closest(".sapUiSizeCompact").length > 0){
-    				this._dialogBaixaEstoque.addStyleClass("sapUiSizeCompact") 
+    				this._dialogCadastro.addStyleClass("sapUiSizeCompact") 
     			} else { 
-    				this._dialogBaixaEstoque.addStyleClass("sapUiSizeCozy")
+    				this._dialogCadastro.addStyleClass("sapUiSizeCozy")
     			}
             	
-            	this.getView().addDependent(this._dialogBaixaEstoque)
+            	this.getView().addDependent(this._dialogCadastro)
             	
-            	this._dialogBaixaEstoque.open()
+            	this._dialogCadastro.open()
             },
 
             onConfirmar : function(oEvent){
 
+                let oModelSend = this.oCadastroModelLine.getData()
+        	
+                this.getOwnerComponent().getModel('oCadastroModel').create('/CadastroSet',oModelSend,{
+                    success:function(oData,oResponse){
+                        //dialog.close()
+                        //this.onClearMessage()
+                        oData.ReturnMessageSet.results.forEach(item=>{
+                            //this.onMessage(this,item.Type === 'S' ? sap.ui.core.MessageType.Success : sap.ui.core.MessageType.Error, item.Message)
+                            //this.getView().getModel("oMessage").updateBindings()
+                            
+                        })
+                    }.bind(this),
+                    error:function(oData,oResponse){
+                       // dialog.close()
+                    }.bind(this)
+                })	
+
+                this._dialogCadastro.close()
             },
 
             onCancelar : function(oEvent){
-
+                this._dialogCadastro.close()
             },
 
             onAfterClose : function(oEvent){
-                
+                this._dialogCadastro.destroy()
             }
 
         });
